3b3f6569ef667deadf62722a1b013423a07e6cbf
ttsoares <ttsoares@orion.ufrgs.br>
2022-05-03 11:22:41 -0300

    URL for heroku

--- src/services/URL.js
+++ src/services/URL.js
@@ -1,2 +1,2 @@
-export const url = "http://127.0.0.1:8888";
-//export const url = "https://thomas-be.herokuapp.com";
+//export const url = "http://127.0.0.1:8888";
+export const url = "https://thomas-be.herokuapp.com";
